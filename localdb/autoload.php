82e7eb4009e9c24511910d139a28e94410642c5a
nicolas.choquet <nicolas.choquet@lagardere-active.com>
2019-07-05 14:46:48 +0200

    add selectors posibilities and demo for this feature add keys and count selectors

--- localdb/autoload.php
+++ localdb/autoload.php
@@ -1,11 +1,11 @@
 <?php
 
-require_once __DIR__.'/classes/ConnectorResult.php';
-require_once __DIR__.'/classes/Connector.php';
-require_once __DIR__.'/classes/Client.php';
-require_once __DIR__.'/classes/Collection.php';
-require_once __DIR__.'/classes/Model.php';
-require_once __DIR__.'/classes/NoSQLSelector.php';
+require_once __DIR__.'/classes/core/ConnectorResult.php';
+require_once __DIR__.'/classes/core/Connector.php';
+require_once __DIR__.'/classes/core/Client.php';
+require_once __DIR__.'/classes/core/Collection.php';
+require_once __DIR__.'/classes/core/Model.php';
+require_once __DIR__.'/classes/core/NoSQLSelector.php';
 require_once __DIR__.'/classes/selectors/keys.php';
 
 
